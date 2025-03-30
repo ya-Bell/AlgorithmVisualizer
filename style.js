263ef29b9bac9839f63bfa60b918d84ab1a2c054
Slava <slavapertsev1337@gmail.com>
2025-03-30 18:33:42 +0700

    add main list and buttons

---
+++ style.js
@@ -0,0 +1,14 @@
+function loadAlgorithm(algorithm) {
+    const contentDiv = document.getElementById("content");
+
+    const algorithms = {
+        "a_star": "<h2>Алгоритм A*</h2><p>ААААА</p>",
+        "clustering": "<h2>Кластеризация</h2><p>БББББ</p>",
+        "genetics": "<h2>Генетические алгоритмы</h2><p>ВВВВВ</p>",
+        "ants": "<h2>Муравьиные алгоритмы</h2><p>ГГГГГ</p>",
+        "solution_tree": "<h2>Дерево решений</h2><p>ДДДДД</p>",
+        "neural_network": "<h2>Нейронные сети</h2><p>ЕЕЕЕЕ</p>",
+    };
+
+    contentDiv.innerHTML = algorithms[algorithm];
+}
